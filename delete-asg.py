34aa081652a047a2addc40591741cc72899d0176
Mohammed Aslaan <81412984+Mohammedaslaan@users.noreply.github.com>
2024-06-13 15:48:40 +0530

    Update delete-asg.py

--- delete-asg.py
+++ delete-asg.py
@@ -1,6 +1,6 @@
 import boto3
 import sys
-# this should reflect in another branch
+# this should reflect in another branch again
 
 def delete_auto_scaling_group_with_name_containing(substring):
     # Initialize the Boto3 Auto Scaling client
